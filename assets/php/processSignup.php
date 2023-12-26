6328da7f000c04e79a4673ff69e330b9799772bd
Biraj Buddhacharya <otaku000god@gmail.com>
2023-12-26 22:10:20 +0545

    process signup (inserting data)

--- assets/php/processSignup.php
+++ assets/php/processSignup.php
@@ -9,7 +9,7 @@
     } 
     
     $dbname = "abc_books";
-    $sqlcreatedb = "CREATE DATABASE IF NOT EXIST $dbname";
+    $sqlcreatedb = "CREATE DATABASE IF NOT EXISTS $dbname";
     if($conn->query($sqlcreatedb) === true){
         echo "Database create successfully <br>";
     }
@@ -35,6 +35,24 @@
     }
     else {
         echo "Error creating table". $conn->error . "<br>";
+    }
+    
+    if($_SERVER["REQUET_METHOD"] == "POST"){
+        $name = $_POST["firstname"] + $_POST("lastname");
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $email = $_POST["email"];
+        $gender = $_POST["gender"];
+        $dob = $_POST["dob"];
+
+        $sqlinsert = "INSERT INTO $tbname (name, username, password, email, gender, dob) VALUES ($name, $username, $password, $email, $gender, $dob) ";
+        if($conn->query($sqlinsert) === true){
+            echo "Data inserted successfuly";
+        }
+        else {
+            echo "Error inserting data". $conn->error . "<br>";
+        }
+        
     }
     
 ?>
